350674d8469d510796a1a856d560b7e6f7e97d26
MinhajSadik <MinhajoSadik@gmail.com>
2021-09-16 02:56:12 +0600

    [add] create pages and home, about,index.css, react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './Index.css';
+import About from './pages/About';
+import Home from './pages/Home';
 
 const App = () => {
-    return (
-        <div>
-            Minhaj-Sadik
-        </div>
-    );
+    return (<>
+
+        <Router>
+            <Switch>
+                <Route path="/" component={Home}></Route>
+                <Route path="about" component={About}></Route>
+            </Switch>
+        </Router>
+
+
+        </>);
 };
 
 export default App;
