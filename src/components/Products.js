ea3138d01ae2d86731c5186a62258fb701093635
MinhajSadik <MinhajoSadik@gmail.com>
2021-09-16 07:12:16 +0600

    [ADD] so much working done

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Product from '../components/Product';
 
 const Products = () => {
     return (
-        <div>
-            This is the product list
+        <div className="container mx-auto pb-24">
+            <h1 className="text-lg font-bold my-8">Products</h1>
+            <div className="grid grid-cols-5 my-8 gap-24">
+               <Product/>
+               <Product/>
+               <Product/>
+               <Product/>
+               <Product/>
+               <Product/>
+               <Product/>
+               <Product/>
+               <Product/>
+               <Product/>
+               <Product/>
+            </div>
         </div>
     );
 };
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Products = () => {
     return (
         <div>
-            This is the product
+            This is Products List
         </div>
     );
 };
